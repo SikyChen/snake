169b92e21afd82e8924508b2b40c26d5b59f688b
chensiqi <chensiqi@cvte.com>
2021-06-17 20:14:07 +0800

    add stamp

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   entry: './src/index.ts',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
+    filename: 'bundle.js?tamp=' + new Date().getTime(),
 
     environment: {
       arrowFunction: false,
